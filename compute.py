31978ebb51e6c8fab4b4f27bf4a5a13a24067093
vrutika03 <59534606+vrutika03@users.noreply.github.com>
2023-05-25 21:55:07 -0300

    Update compute.py
    working

--- compute.py
+++ compute.py
@@ -1,36 +1,3 @@
-# import sys
-
-# def compute_values(threshold, limit):
-#     output = []
-#     cumulative_sum = 0.0
-
-#     with open('input.txt', 'r') as file:
-#         for line in file:
-#             value = float(line.strip())
-        
-#             if value < threshold:
-#                 transformed_value = 0.0
-#             elif value > limit:
-#                 transformed_value = limit
-#             else:
-#                 transformed_value = value
-        
-#             output.append(transformed_value)
-#             cumulative_sum += transformed_value
-#             sys.stdout.write(str(transformed_value) + "\n")
-    
-#     sys.stdout.write(str(cumulative_sum) + "\n")
-
-# if __name__ == "__main__":
-#     if len(sys.argv) != 3:
-#         sys.stderr.write("Error: Two numerical arguments (threshold and limit) are required.\n")
-#         sys.exit(1)
-    
-#     threshold = float(sys.argv[1])
-#     limit = float(sys.argv[2])
-    
-#     compute_values(threshold, limit)
-
 
 import sys
 
